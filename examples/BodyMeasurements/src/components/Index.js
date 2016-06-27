c22de03ac5739a504f043b7c542ba062a53053f0
Greg Wilson <gregwilson@outlook.com>
2016-06-27 00:57:51 -0400

    added BodyMeasurements example app to examples... WIP

---
+++ examples/BodyMeasurements/src/components/Index.js
@@ -0,0 +1,49 @@
+/**
+ * Created by greg on 2016-06-27.
+ */
+
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Navigator,
+    Text,
+    View
+} from 'react-native';
+
+
+class Index extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Index
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+
+module.exports = Index;
+export default Index;
