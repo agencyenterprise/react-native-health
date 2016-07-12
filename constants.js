48d60197d1f28865d303390628c8709ed4972dfe
Daniele Volpi <daniele.volpi@medx.net>
2016-07-12 10:53:31 +0200

    updated constanst

--- constants.js
+++ constants.js
@@ -19,6 +19,12 @@ const PERMISSIONS = {
 	NikeFuel: 'NikeFuel',
 	AppleExerciseTime: 'AppleExerciseTime',
 	DietaryEnergy: 'DietaryEnergy',
+	HeartRate: 'HeartRate',
+  BodyTemperature: 'BodyTemperature',
+  BloodPressureSystolic: 'BloodPressureSystolic',
+  BloodPressureDiastolic: 'BloodPressureDiastolic',
+  RespiratoryRate: 'RespiratoryRate',
+  BloodGlucose: 'BloodGlucose',
 };
 
 
