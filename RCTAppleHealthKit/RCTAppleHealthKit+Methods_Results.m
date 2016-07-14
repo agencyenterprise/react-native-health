bcbaf22e023f37a5bf1afe59d12b39df1342f40a
Daniele Volpi <daniele.volpi@medx.net>
2016-07-14 11:36:59 +0200

    added results identifier declarations

---
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Results.m
@@ -0,0 +1,10 @@
+#import "RCTAppleHealthKit+Methods_Results.h"
+#import "RCTAppleHealthKit+Queries.h"
+#import "RCTAppleHealthKit+Utils.h"
+
+@implementation RCTAppleHealthKit (Methods_Results)
+
+
+
+
+@end
