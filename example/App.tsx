5505acd77fc2bfa0e1f03f64e1baf619348679b3
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-03-15 14:18:02 -0300

    lint: fix App code style

--- example/App.tsx
+++ example/App.tsx
@@ -1,14 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -18,55 +8,52 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import AppleHealthKit, { HealthValue, HealthKitPermissions } from 'react-native-health';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import AppleHealthKit, {
+  HealthValue,
+  HealthKitPermissions,
+} from 'react-native-health';
 
 /* Permission options */
 const permissions = {
   permissions: {
-    read: [
-      AppleHealthKit.Constants.Permissions.HeartRate,
-    ],
-    write: [
-      AppleHealthKit.Constants.Permissions.Steps,
-    ],
-  }
-} as HealthKitPermissions
-
+    read: [AppleHealthKit.Constants.Permissions.HeartRate],
+    write: [AppleHealthKit.Constants.Permissions.Steps],
+  },
+} as HealthKitPermissions;
 
 AppleHealthKit.initHealthKit(permissions, (error: string) => {
   /* Called after we receive a response from the system */
 
   if (error) {
-   console.log('[ERROR] Cannot grant permissions!')
+    console.log('[ERROR] Cannot grant permissions!');
   }
 
   /* Can now read or write to HealthKit */
 
   const options = {
-    startDate: (new Date(2020, 1, 1)).toISOString(),
-  }
+    startDate: new Date(2020, 1, 1).toISOString(),
+  };
 
-  AppleHealthKit.getHeartRateSamples(options, (callbackError: string, results: HealthValue[]) => {
-    /* Samples are now collected from HealthKit */
-  });
-})
+  AppleHealthKit.getHeartRateSamples(
+    options,
+    (callbackError: string, results: HealthValue[]) => {
+      /* Samples are now collected from HealthKit */
+    },
+  );
+});
 
 export default function App() {
   const [authStatus, setAuthStatus] = useState<any>({});
 
   const handlePressGetAuthStatus = () => {
     AppleHealthKit.getAuthStatus(permissions, (err, result) => {
-      if (err) console.error(err);
+      if (err) {
+        console.error(err);
+      }
       setAuthStatus(result);
-    })
-  } 
+    });
+  };
 
   return (
     <>
@@ -77,8 +64,12 @@ export default function App() {
           style={styles.scrollView}>
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>React Native Health Example</Text>
-              <Text onPress={handlePressGetAuthStatus}>Press me to get Auth Status</Text>
+              <Text style={styles.sectionTitle}>
+                React Native Health Example
+              </Text>
+              <Text onPress={handlePressGetAuthStatus}>
+                Press me to get Auth Status
+              </Text>
               <Text style={styles.sectionDescription}>
                 {JSON.stringify(authStatus, null, 2)}
               </Text>
@@ -88,7 +79,7 @@ export default function App() {
       </SafeAreaView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   scrollView: {
