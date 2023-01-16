504deda881e2f2367bf75d436de09d41afd19aa7
Gustavo Gava <gustavogarciagava@gmail.com>
2023-01-16 14:00:41 -0300

    - Updated example app react and react-native version - Added example of how to enable listeners - Updated required react-native version on lib's package.json

--- example/index.js
+++ example/index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, NativeEventEmitter, NativeModules } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => {
+    new NativeEventEmitter(NativeModules.AppleHealthKit).addListener('healthKit:HeartRate:new', async () => {
+        console.log('new sample')
+    })
+    return App
+});
