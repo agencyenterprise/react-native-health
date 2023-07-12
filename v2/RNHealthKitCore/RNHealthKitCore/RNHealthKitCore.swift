9c12579976b4b5b1262822e2d5c3354305d629f7
Gustavo Gava <gustavogarciagava@gmail.com>
2023-07-12 11:46:06 +0200

    - Added metadata field to samples - Samples are now encodable to String for bridging to react native

--- v2/RNHealthKitCore/RNHealthKitCore/RNHealthKitCore.swift
+++ v2/RNHealthKitCore/RNHealthKitCore/RNHealthKitCore.swift
@@ -75,30 +75,3 @@ public class RNHealthKitCore {
         }
     }
 }
-
-public struct QuantitySample {
-    let id: String?
-    let value: Double
-    let startDate: Date
-    let endDate: Date
-    let sourceName: String?
-    let sourceId: String?
-    
-    init(_ sample: HKQuantitySample, _ unit: HKUnit) {
-        self.id = sample.uuid.uuidString
-        self.value = sample.quantity.doubleValue(for: unit)
-        self.startDate = sample.startDate
-        self.endDate = sample.endDate
-        self.sourceName = sample.sourceRevision.source.name
-        self.sourceId = sample.sourceRevision.source.bundleIdentifier
-    }
-
-    init(_ quantity: HKQuantity, _ unit: HKUnit, _ startDate: Date, _ endDate: Date) {
-        self.value = quantity.doubleValue(for: unit)
-        self.startDate = startDate
-        self.endDate = startDate
-        self.id = nil
-        self.sourceName = nil
-        self.sourceId = nil
-    }
-}
