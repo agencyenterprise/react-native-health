6873f2dd78af1e6196e7d6e30bef26f47a8ff2cd
Gustavo Gava <gustavogarciagava@gmail.com>
2023-08-23 17:55:39 -0300

    - Sample dates are now string

--- v2/RNHealthKitCore/HealthKitCore/Samples.swift
+++ v2/RNHealthKitCore/HealthKitCore/Samples.swift
@@ -4,8 +4,8 @@ import HealthKit
 public struct QuantitySample: Encodable {
     public let id: String?
     public let value: Double
-    public let startDate: Date
-    public let endDate: Date
+    public let startDate: String
+    public let endDate: String
     public let sourceName: String?
     public let sourceId: String?
     public let metadata: [String: String]?
@@ -13,8 +13,8 @@ public struct QuantitySample: Encodable {
     init(_ sample: HKQuantitySample, _ unit: HKUnit) {
         self.id = sample.uuid.uuidString
         self.value = sample.quantity.doubleValue(for: unit)
-        self.startDate = sample.startDate
-        self.endDate = sample.endDate
+        self.startDate = sample.startDate.toIsoString() ?? ""
+        self.endDate = sample.endDate.toIsoString() ?? ""
         self.sourceName = sample.sourceRevision.source.name
         self.sourceId = sample.sourceRevision.source.bundleIdentifier
         self.metadata = sample.metadata?.mapValues(String.init(describing:)) ?? [:]
@@ -22,8 +22,8 @@ public struct QuantitySample: Encodable {
 
     init(_ quantity: HKQuantity, _ unit: HKUnit, _ startDate: Date, _ endDate: Date) {
         self.value = quantity.doubleValue(for: unit)
-        self.startDate = startDate
-        self.endDate = startDate
+        self.startDate = startDate.toIsoString() ?? ""
+        self.endDate = endDate.toIsoString() ?? ""
         self.id = nil
         self.sourceName = nil
         self.sourceId = nil
