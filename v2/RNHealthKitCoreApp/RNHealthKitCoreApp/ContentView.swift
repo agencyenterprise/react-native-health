82c24991d58f64524da4e6f89bacba54fde6a1a1
Thalys Viana <thalysvianaguiar@gmail.com>
2023-11-27 22:47:37 -0300

    Refactor workout creation to use new workout api

--- v2/RNHealthKitCoreApp/RNHealthKitCoreApp/ContentView.swift
+++ v2/RNHealthKitCoreApp/RNHealthKitCoreApp/ContentView.swift
@@ -13,96 +13,34 @@ struct ContentView: View {
         .padding()
         .onAppear {
             Task {
-                let core = try! await HealthKitCore(
-                    read: [
-                        WorkoutType.workout,
-                    ],
-                    write: [WorkoutType.workout]
-                )
+                do {
+                    let core = try await HealthKitCore(
+                        read: [WorkoutType.workout],
+                        write: [WorkoutType.workout]
+                    )
+                    if HKHealthStore.isHealthDataAvailable() {
+//                        await saveWorkoutWithMetadata(core: core)
+                        dump(try await core.getCompletedWorkouts(queryParameters: .init()))
+                    }
+                } catch {
+                    print("Error occurred: \(error)")
+                }
 
-                //print(
-                //    try! await core.getQuantitySamples(.HeartRate, .init(startDate: Calendar(identifier: .gregorian).date(byAdding: .year, value: -2, to: .now)!, endDate: Date(), unit: .count().unitDivided(by: .minute())))
-                //)
-
-                /*
-                 print(
-                 try! await core.getCompletedWorkouts(
-                 queryParameters: .init(
-                 startDate: Calendar(identifier: .gregorian).date(byAdding: .year, value: -2, to: .now)!,
-                 endDate: nil,
-                 activityTypes: [37],
-                 ids: nil,
-                 isUserEntered: nil
-                 )
-                 ))
-                 */
-
-    //            print(try! await core.saveCompletedWorkout(
-    //                activityType: .americanFootball,
-    //                startDate: Calendar(identifier: .gregorian).date(byAdding: .minute, value: -2, to: .now)!,
-    //                endDate: Date(),
-    //                totalEnergyBurned: 120,
-    //                totalDistance: 2000,
-    //                metadata: ["HKMetadataKeyIndoorWorkout": true]
-    //            ))
-
-
-    //            await saveWorkoutWithMetadata(core: core)
-
-                print(try! await core.getCompletedWorkouts(queryParameters: .init()))
-
-    //            print(try! await core.getCompletedWorkouts(queryParameters: .init()))
-
-
-                //         print(
-                //            try! await core.getQuantitySamplesStatistics(
-                //                .ActiveEnergyBurned,
-                //                StatisticsQuantityQuery(
-                //                    startDate: Calendar(identifier: .gregorian).date(byAdding: .year, value: -2, to: .now)!,
-                //                    endDate: .now,
-                //                    interval: .init(month: 1),
-                //                    anchorDate: Calendar(identifier: .gregorian).date(byAdding: .month, value: -12, to: .now)!,
-                //                    unit: .kilocalorie(),
-                //                    statisticsOption: .cumulativeSum,
-                //                    isUserEntered: nil
-                //                )
-                //            )
-                //         )
-
-
-                /*
-                 try! await core.saveQuantitySample(
-                 .init(
-                 type: .HeartRate,
-                 value: 69,
-                 unit: "count/min",
-                 startDate: Calendar(identifier: .gregorian).date(byAdding: .minute, value: -1, to: .now)!,
-                 endDate: Calendar(identifier: .gregorian).date(byAdding: .minute, value: -1, to: .now)!,
-                 metadata: ["HKMetadataKeyHeartRateMotionContext": 1]
-                 )
-                 )
-                 */
             }
         }
     }
 
     func saveWorkoutWithMetadata(core: HealthKitCore) async {
         let unit = HKUnit(from: "min")
-        let quantity = HKQuantity(unit: unit, doubleValue: 30)
-        print("HEY THERE: \(quantity)")
         let rawMetadata: [String: Any] = [
             "HKIndoorWorkout": true,
             "HKFitnessMachineDuration": ["unit": "min", "doubleValue": 30.0],
-            "HKWaterSalinity": 0 // Assuming 0 represents freshWater
-            // Add other metadata fields as needed
         ]
 
-        let processedMetadata = try? WorkoutHelper.processWorkoutMetadata(rawMetadata)
-        
-        print("ProcessedMetadata: \(processedMetadata)")
+        let processedMetadata = try! WorkoutHelper.processWorkoutMetadata(rawMetadata)
 
-        print(try! await core.saveCompletedWorkout(
-            activityType: .americanFootball,
+        dump(try! await core.saveCompletedWorkout(
+            activityType: .coreTraining,
             startDate: Calendar(identifier: .gregorian).date(byAdding: .minute, value: -2, to: .now)!,
             endDate: Date(),
             totalEnergyBurned: 120,
